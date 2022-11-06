fd4bf7854e7a86c4d16a9e3ca392c90bea2df0f5
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-11-06 21:52:31 +0300

    Initial commit

---
+++ app/src/main/java/com/thechance/whatschance/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thechance.whatschance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
