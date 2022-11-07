0f8080a0deda6e85367bd5de57b1486e709a514c
Nada Feteiha <nada.feteiha@gmail.com>
2022-11-07 00:34:48 -0800

    implement chat ui

---
+++ app/src/main/java/com/thechance/whatschance/WhatsChanceApp.kt
@@ -0,0 +1,7 @@
+package com.thechance.whatschance
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WhatsChanceApp : Application()
--- app/src/main/java/com/thechance/whatschance/ui/main/MainActivity.kt
+++ app/src/main/java/com/thechance/whatschance/ui/main/MainActivity.kt
@@ -3,7 +3,9 @@ package com.thechance.whatschance.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.thechance.whatschance.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
