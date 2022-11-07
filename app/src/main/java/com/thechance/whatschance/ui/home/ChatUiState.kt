0fc547a922b4077a748973ad2eabbf938326afdc
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-11-08 00:19:13 +0300

    create chat ui state

---
+++ app/src/main/java/com/thechance/whatschance/ui/home/ChatUiState.kt
@@ -0,0 +1,8 @@
+package com.thechance.whatschance.ui.home
+
+data class ChatUiState(
+    val id: String,
+    val name: String,
+    val lastMessage: String,
+    val lastMessageTime: String,
+)
--- app/src/main/java/com/thechance/whatschance/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/thechance/whatschance/ui/home/HomeAdapter.kt
@@ -4,16 +4,9 @@ import com.thechance.whatschance.BaseAdapter
 import com.thechance.whatschance.BaseInteractionListener
 import com.thechance.whatschance.R
 
-class HomeAdapter(items: List<Chat>, listener: ChatsAdapterListener) :
-    BaseAdapter<Chat>(items = items, listener = listener) {
+class HomeAdapter(items: List<ChatUiState>, listener: ChatsAdapterListener) :
+    BaseAdapter<ChatUiState>(items = items, listener = listener) {
     override val layoutID: Int = R.layout.item_chat
 }
-
-data class Chat(
-    val id: String,
-    val name: String,
-    val lastMessage: String,
-    val lastMessageTime: String,
-)
 
 interface ChatsAdapterListener : BaseInteractionListener
