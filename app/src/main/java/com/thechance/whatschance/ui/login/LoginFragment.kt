8a21f9b60b107aad5b496c60a8a426ee8bfca823
Ahmad Batal <ahmad77a44k@gmail.com>
2022-11-07 18:54:03 +0200

    implement login screen

---
+++ app/src/main/java/com/thechance/whatschance/ui/login/LoginFragment.kt
@@ -0,0 +1,21 @@
+package com.thechance.whatschance.ui.login
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.thechance.whatschance.R
+import com.thechance.whatschance.databinding.FragmentLoginBinding
+import com.thechance.whatschance.ui.base.BaseFragment
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+
+    override val layoutIdFragment = R.layout.fragment_login
+    override val viewModel: LoginViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
