aca781710eaae4d433531e1d3650b4e3343a6dc2
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-11-08 22:28:23 +0300

    fix bugs

--- app/src/main/java/com/thechance/whatschance/ui/VerificationFragment.kt
+++ app/src/main/java/com/thechance/whatschance/ui/VerificationFragment.kt
@@ -5,9 +5,10 @@ import androidx.lifecycle.ViewModel
 import com.thechance.whatschance.R
 import com.thechance.whatschance.databinding.FragmentVerificationBinding
 import com.thechance.whatschance.ui.base.BaseFragment
+import com.thechance.whatschance.ui.contact.ContactViewModel
 
 
 class VerificationFragment : BaseFragment<FragmentVerificationBinding>() {
     override val layoutIdFragment  = R.layout.fragment_verification
-    override val viewModel: ViewModel by viewModels()
+    override val viewModel: ContactViewModel by viewModels()
 }
--- app/src/main/java/com/thechance/whatschance/ui/VerificationViewModel.kt
+++ app/src/main/java/com/thechance/whatschance/ui/VerificationViewModel.kt
@@ -1,9 +1,8 @@
-package com.thechance.whatschance.ui.contact
+package com.thechance.whatschance.ui
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ContactViewModel @Inject constructor() : ViewModel() {
-}
+class VerificationViewModel @Inject constructor(): ViewModel()
