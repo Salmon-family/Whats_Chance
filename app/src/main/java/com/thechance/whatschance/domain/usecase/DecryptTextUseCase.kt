f39112abae4a65e40f60627707980a56b82c0e57
Falah Hassan <devfalahhassan@gmail.com>
2022-11-09 21:08:39 -0800

    refactor code

--- app/src/main/java/com/thechance/whatschance/domain/usecase/DecryptTextUseCase.kt
+++ app/src/main/java/com/thechance/whatschance/domain/usecase/DecryptTextUseCase.kt
@@ -1,12 +1,13 @@
 package com.thechance.whatschance.domain.usecase
 
+import com.thechance.whatschance.domain.DataEncryptor
 import com.thechance.whatschance.domain.DataEncryptorImp
 import javax.inject.Inject
 
 class DecryptTextUseCase @Inject constructor(
-    private val cryptoManger: DataEncryptorImp,
+    private val dataEncryptor: DataEncryptor,
 ) {
     operator fun invoke(cipherText:String):String{
-        return cryptoManger.decrypt(cipherText)
+        return dataEncryptor.decrypt(cipherText)
     }
 }
--- app/src/main/java/com/thechance/whatschance/domain/usecase/EncryptTextUseCase.kt
+++ app/src/main/java/com/thechance/whatschance/domain/usecase/EncryptTextUseCase.kt
@@ -1,12 +1,13 @@
 package com.thechance.whatschance.domain.usecase
 
+import com.thechance.whatschance.domain.DataEncryptor
 import com.thechance.whatschance.domain.DataEncryptorImp
 import javax.inject.Inject
 
 class EncryptTextUseCase @Inject constructor(
-    private val cryptoManger: DataEncryptorImp,
+    private val dataEncryptor: DataEncryptor,
 ) {
     operator fun invoke(plainText:String):String{
-        return cryptoManger.encrypt(plainText)
+        return dataEncryptor.encrypt(plainText)
     }
 }
