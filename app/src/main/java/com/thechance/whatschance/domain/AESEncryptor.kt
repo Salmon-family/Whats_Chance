25c56c591da4d3084cdeaf3fbac5d31fba4f6ffe
Falah Hassan <devfalahhassan@gmail.com>
2022-11-09 23:30:45 -0800

    refactor code

--- app/src/main/java/com/thechance/whatschance/domain/AESEncryptor.kt
+++ app/src/main/java/com/thechance/whatschance/domain/AESEncryptor.kt
@@ -18,16 +18,16 @@ class AESEncryptor @Inject constructor(
     private val cipher: Cipher,
     private val ivParameterSpec: IvParameterSpec,
     private val secretKey: SecretKeySpec
-) : DataEncryptor{
+) : DataEncryptor {
 
-    override fun encrypt(plainText : String): String {
+    override fun encrypt(plainText: String): String {
         cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParameterSpec)
         val encrypted = cipher.doFinal(plainText.toByteArray())
         val encodedByte = Base64.encode(encrypted, Base64.DEFAULT)
         return String(encodedByte)
     }
 
-    override fun decrypt(cipherText:String): String {
+    override fun decrypt(cipherText: String): String {
         val decodedByte: ByteArray = Base64.decode(cipherText, Base64.DEFAULT)
         cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec)
         val plainText = cipher.doFinal(decodedByte)
