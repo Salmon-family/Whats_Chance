63d79d9a190a2e2abddd6934931df99ceb81b72e
Nada Feteiha <nada.feteiha@gmail.com>
2022-11-10 14:13:51 -0800

    fix conflict

--- app/src/main/java/com/thechance/whatschance/ui/VerificationViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.thechance.whatschance.ui
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class VerificationViewModel @Inject constructor(): ViewModel()
