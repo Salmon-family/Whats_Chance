ed3f68e3efc4f033744c21b5467f8eb78ccbb3a0
Falah Hassan <devfalahhassan@gmail.com>
2022-11-13 22:10:37 -0800

    add constant to collection key

--- app/src/main/java/com/thechance/whatschance/data/FireStoreDataSource.kt
+++ app/src/main/java/com/thechance/whatschance/data/FireStoreDataSource.kt
@@ -11,15 +11,25 @@ class FireStoreDataSource @Inject constructor(
     private val fireStore: FirebaseFirestore
 ) {
     fun getUsers(uId: String) : Flow<QuerySnapshot> {
-        return fireStore.collection("devfalahUsers").whereNotEqualTo("uId",uId).snapshots()
+        return fireStore.collection(USERS_COLLECTION).whereNotEqualTo(U_ID_KEY,uId).snapshots()
     }
 
     fun addMessage(uId : String,message: MessageDto) : Boolean {
-        val messagesRef = fireStore.collection("devfalahMessages").document(uId).collection("messages")
+        val messagesRef = fireStore.collection(MESSAGES_COLLECTION).document(uId).collection(MESSAGE_COLLECTION)
         return messagesRef.add(message).isSuccessful
     }
 
     fun getMessages(uId: String,senderId: String) : Flow<QuerySnapshot>{
-        return fireStore.collection("devfalahMessages").document(uId).collection("messages").whereEqualTo("sender",senderId).snapshots()
+        return fireStore.collection(MESSAGES_COLLECTION).document(uId).collection(MESSAGE_COLLECTION).whereEqualTo(
+            SENDER_ID_KEY,senderId).snapshots()
+    }
+
+
+    companion object{
+        private const val U_ID_KEY = "uId"
+        private const val SENDER_ID_KEY = "sender"
+        private const val USERS_COLLECTION = "devfalahUsers"
+        private const val MESSAGES_COLLECTION = "devfalahMessages"
+        private const val MESSAGE_COLLECTION = "messages"
     }
 }
--- app/src/main/java/com/thechance/whatschance/data/response/MessageDto.kt
+++ app/src/main/java/com/thechance/whatschance/data/response/MessageDto.kt
@@ -1,10 +1,8 @@
 package com.thechance.whatschance.data.response
 
-import java.util.Date
 
-data class MessageDto (
+data class MessageDto(
     val id: String = "",
     val textMessage: String = "",
     val sender: String = "",
-
-        )
+    )
