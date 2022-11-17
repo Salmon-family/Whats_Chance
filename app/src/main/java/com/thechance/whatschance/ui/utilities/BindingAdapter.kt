94128a3c1d86643ba4a2cfdd6766cbd0f0337d1a
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-11-17 22:45:20 +0300

    reverse chat recycler view

--- app/src/main/java/com/thechance/whatschance/ui/utilities/BindingAdapter.kt
+++ app/src/main/java/com/thechance/whatschance/ui/utilities/BindingAdapter.kt
@@ -10,6 +10,7 @@ import com.thechance.whatschance.ui.base.BaseAdapter
 @BindingAdapter(value = ["app:items"])
 fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
     (view.adapter as BaseAdapter<T>?)?.setItems(items ?: emptyList())
+    view.scrollToPosition(0)
 
 }
 
