3237064b4137e9d04725c4d0ae1f0059ee585198
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-11-20 01:48:50 +0300

    setup action bar with navigation controller

--- app/src/main/java/com/thechance/whatschance/ui/main/WhatsChanceActivity.kt
+++ app/src/main/java/com/thechance/whatschance/ui/main/WhatsChanceActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.thechance.whatschance.R
 import com.thechance.whatschance.databinding.ActivityWhatsChanceBinding
 import com.thechance.whatschance.ui.authentication.AuthenticationActivity
@@ -17,13 +19,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class WhatsChanceActivity : BaseActivity<ActivityWhatsChanceBinding>() {
     override val viewModel: WhatsChanceViewModel by viewModels()
     override val layoutIdActivity: Int = R.layout.activity_whats_chance
-
-    override fun onStart() {
-        super.onStart()
-        window.statusBarColor = Color.parseColor(viewModel.brandColor.value)
-        actionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor(viewModel.brandColor.value)))
-        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor(viewModel.brandColor.value)))
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
@@ -40,5 +35,15 @@ class WhatsChanceActivity : BaseActivity<ActivityWhatsChanceBinding>() {
             this.finish()
         }
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp() || super.onSupportNavigateUp()
     }
 }
